72fc154128d959e99c12d03a5f5f6ced0d405582
Shankar Modi <shankar.modi08@gmail.com>
2023-11-28 22:03:33 +0530

    Day 4 : Print only once sub sequence with sum k

--- src/src/com/sk/recursion/SubSequenceWIthSumK.java
+++ src/src/com/sk/recursion/SubSequenceWIthSumK.java
@@ -8,6 +8,7 @@ public class SubSequenceWIthSumK {
         int[] arr = {3,1,2,2,1};
         int sum = 3;
         printSubSeqWithSumK(arr, sum, 0,new ArrayList<Integer>());
+        print1SubSeqWithSumK(arr, sum, 0,new ArrayList<Integer>());
     }
 
     private static void printSubSeqWithSumK(int[] arr, int sum, int i, ArrayList<Integer> list) {
@@ -22,5 +23,23 @@ public class SubSequenceWIthSumK {
         printSubSeqWithSumK(arr, sum-arr[i],i+1, list);
         list.removeLast();
         printSubSeqWithSumK(arr, sum,i+1, list);
+    }
+
+    private static boolean print1SubSeqWithSumK(int[] arr, int sum, int i, ArrayList<Integer> list){
+        if (arr.length == i){
+            if (sum==0){
+                System.out.println(" 1 SEQ : " + list );
+                return true;
+            }
+            return false;
+        }
+
+        list.add(arr[i]);
+        if(print1SubSeqWithSumK(arr, sum-arr[i], i+1, list)){
+            return true;
+        }
+        list.removeLast();
+        return print1SubSeqWithSumK(arr, sum, i+1, list);
+
     }
 }
